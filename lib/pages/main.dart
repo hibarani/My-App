e8d86f2a523c58b28523f7ba72c1fe924810ba2c
YourUsername <your_email@example.com>
2026-07-10 06:24:37 -0700

    title

--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -21,6 +21,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+    
+    title: "Firebase",
       debugShowCheckedModeBanner: false,
       home: Login(),
     );
